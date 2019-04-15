fb3d35ca643e686b78ac985e09b2fcefa85a9b48
liye45 <1412154147@qq.com>
2019-04-15 15:05:40 +0800

    password

---
+++ 11-password-visual/13-password.js
@@ -0,0 +1,22 @@
+
+$(function(){
+    var $pwd     = $('#pwd'),
+              $pwdText = $('.pwd-text'),
+                    $eye     = $('#eye');
+
+    $pwd.on('input', function(){
+          $pwdText.val($pwd.val());
+            
+    });
+
+    $eye.mouseover(function(){
+          $pwdText.css('z-index', '10');
+            
+    });
+
+    $eye.mouseout(function(){
+          $pwdText.css('z-index', '-10');
+            
+    });
+
+});
